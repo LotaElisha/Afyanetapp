5ad045c6448614493ef67bedd01a47bdcc40c4af
azeez1776 <aboud3am81@gmail.com>
2022-11-10 19:31:07 +0300

    :construction: minor changes

--- src/redux/actions/OtpAction.js
+++ src/redux/actions/OtpAction.js
@@ -1,34 +1,43 @@
-import {API, BASE_URL} from "../constants";
+import { API, BASE_URL } from "../constants";
 import axios from "axios";
 
 //=====================SEND/RESEND OTP=====================//
 export const SendOtpApi = (payload, sendOtpResponse) => {
-    let data = new FormData();
-    data.append("phone_number", payload.phone_number);
+  let data = new FormData();
+  data.append("phone_number", payload.phone_number);
 
-    axios.post(BASE_URL + API.SEND_OTP_API, data, {
-        headers: {}
-    }).then((response) => {
-        sendOtpResponse(response.data)
+  axios
+    .post(BASE_URL + API.SEND_OTP_API, data, {
+      headers: {},
     })
-        .catch((error) => {
-            sendOtpResponse(error)
-        }).done();
+    .then((response) => {
+      console.log("response1", response);
+      sendOtpResponse(response.data);
+    })
+    .catch((error) => {
+      console.log("error1", error);
+      sendOtpResponse(error);
+    })
+    .done();
 };
-
 
 //=====================VERIFY OTP=====================//
 export const VerifyOtpApi = (payload, verifyOtpResponse) => {
-    let data = new FormData();
-    data.append("phone_number", payload.phone_number);
-    data.append("code", payload.code);
+  let data = new FormData();
+  data.append("phone_number", payload.phone_number);
+  data.append("code", payload.code);
 
-    axios.post(BASE_URL + API.VERIFY_OTP_API, data, {
-        headers: {}
-    }).then((response) => {
-        verifyOtpResponse(response)
+  axios
+    .post(BASE_URL + API.VERIFY_OTP_API, data, {
+      headers: {},
     })
-        .catch((error) => {
-            verifyOtpResponse(error)
-        }).done();
+    .then((response) => {
+      console.log("response2", response);
+      verifyOtpResponse(response);
+    })
+    .catch((error) => {
+      console.log("error2", error);
+      verifyOtpResponse(error);
+    })
+    .done();
 };
